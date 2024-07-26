bacf953645d885c32dbd38c96fb6a720b0ec5d3d
Anshumaan Singh <144586980+asing407@users.noreply.github.com>
2024-07-26 21:49:43 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -6,7 +6,21 @@ const nextConfig = {
   },
   images: {
     domains: ['m.media-amazon.com']
+  },
+  // Add this section to set the maxDuration for your Serverless Function
+  async rewrites() {
+    return [
+      {
+        source: '/api/cron',
+        destination: '/api/cron',
+        async middleware(req, ev) {
+          // Your Serverless Function code here
+          // ...
+        },
+        maxDuration: 30 // Set maxDuration to 30 seconds, for example
+      }
+    ];
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
