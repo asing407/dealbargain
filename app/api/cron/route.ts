d5bcb3539a541f6b44ea85db0390445af905d9ce
Anshumaan Singh <144586980+asing407@users.noreply.github.com>
2024-07-29 00:51:01 +0530

    commit-product

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -44,7 +44,7 @@ export async function GET(request: Request) {
         // Update Products in DB
         const updatedProduct = await Product.findOneAndUpdate(
           {
-            url: Product.url,
+            url: product.url,
           },
           product
         );
